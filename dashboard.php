e4d0fa7067b17a0749f8e3b275a7bef2b86d3bce
Oluwatosin Akinfenwa <atosin056@gmail.com>
2024-10-13 23:58:05 +0100

    feat dashboard almost complete

--- dashboard.php
+++ dashboard.php
@@ -24,7 +24,7 @@
 
 <body>
 
-<!-- <div id="splash-screen">
+<div id="splash-screen">
 
 	<div>
 
@@ -32,19 +32,51 @@
 
 	</div>
 
-</div> -->
+</div>
 
-<!-- <div id="main-content"> -->
+<div id="main-content">
 
-	<div style="padding: 40px 120px;">
+	<div style="padding: 40px 120px;" class="nope">
 
 		<div class="nav">
 			
-			T
+			<div style="display: flex;">
+				
+				<div class="act acti">
+					
+					<img src="images/House.png">
+
+				</div>
+
+				<div class="act">
+					
+					<img src="images/circle.png">
+
+				</div>
+
+				<div class="act">
+					
+					<img src="images/robot.png">
+
+				</div>
+
+				<div class="act">
+					
+					<img src="images/user.png">
+
+				</div>
+
+				<div class="act">
+					
+					<img src="images/gear.png">
+
+				</div>	
+
+			</div>
 
 		</div>
 		
-		<div>
+		<div class="row" style="gap: 20px;">
 			
 			<div class="col-lg-5 col-sm-5 col-md-5" style="background: transparent;padding-top: 30px;padding-top: 58px;">
 				
@@ -70,13 +102,97 @@
 
 							</div>
 
-							<div>
+							<div style="display: flex;flex-direction: column;gap: 10px;margin-top: 40px;">
 								
 								<div class="temp-ch">
 									
-									<div>
+									<div class="row" style="padding: 0 !important;background: transparent;">
 										
-										T
+										<div style="display: inline-block;width: auto;">
+											
+											<img src="images/dash.png">
+
+										</div>
+
+										<div style="display: inline-flex;width: auto;flex-direction: column;gap: 4.5px;padding-top: 4px;padding-left: 0 !important;">
+											
+											<font class="t-read">Temperature Reading</font>
+
+											<font class="re">The surface temperature is 36.6°C</font>
+
+										</div>
+
+									</div>
+
+								</div>
+
+								<!-- End -->
+
+								<div class="temp-ch">
+									
+									<div class="row" style="padding: 0 !important;background: transparent;">
+										
+										<div style="display: inline-block;width: auto;">
+											
+											<img src="images/dash.png">
+
+										</div>
+
+										<div style="display: inline-flex;width: auto;flex-direction: column;gap: 4.5px;padding-top: 4px;padding-left: 0 !important;">
+											
+											<font class="t-read">Temperature Reading</font>
+
+											<font class="re">The surface temperature is 36.6°C</font>
+
+										</div>
+
+									</div>
+
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<!-- End -->
+
+			<div style="flex-grow: 1;background: transparent;width: auto;">
+				
+				<div>
+					
+					<div>
+						
+						<div class="row" style="align-items: center;">
+							
+							<div class="col-lg-7 col-sm-7 col-md-7 heart">
+								
+								<div class="row">
+									
+									<div class="img-cont">
+										
+										<img src="images/heart.png">
+
+									</div>
+
+									<div style="display: inline-block;width: auto;display: flex;align-items: center;justify-content: flex-end;flex-grow: 1;">
+										
+										<font style="color: var(--primary-blue, #007BFF);">21/06/24</font>
+
+									</div>
+
+								</div>
+
+								<div style="flex-grow: 1;background: transparent;display: flex;align-items: flex-end;">
+									
+									<div style="display: flex;align-items: center;gap: 10px;">
+										
+										<font class="big-t">72</font><font class="small-t">BPM</font>
 
 									</div>
 
@@ -94,30 +210,134 @@
 
 		</div>
 
+		<!-- End -->
+
+		<div style="margin-top: 40px;">
+			
+			<div>
+				
+				<div class="row" style="gap: 8px;">
+					
+					<div class="col-lg-4 col-sm-4 col-md-4 tips">
+						
+						<div class="row" style="justify-content: space-between;">
+							
+							<div style="background: transparent;display: flex;flex-direction: column;justify-content: center;" class="col-lg-6 col-sm-6 col-md-6">
+								
+								<p class="health">Health Tips</p>
+
+								<font class="did">Did you know?</font>
+
+								<div style="margin-top: 5px;">
+									
+									<font class="tipe">Washing your hands frequently reduces the risk of infections.</font>
+
+								</div>
+
+							</div>
+
+							<div style="display: inline-block;width: auto;">
+								
+								<img src="images/health.png">
+
+							</div>
+
+						</div>
+
+					</div>
+
+					<!-- End -->
+
+					<div class="col-lg-4 col-sm-4 col-md-4 tips">
+						
+						<div class="row" style="justify-content: space-between;">
+							
+							<div style="background: transparent;display: flex;flex-direction: column;justify-content: center;" class="col-lg-6 col-sm-6 col-md-6">
+								
+								<p class="health">Health Tips</p>
+
+								<font class="did">Did you know?</font>
+
+								<div style="margin-top: 5px;">
+									
+									<font class="tipe">Washing your hands frequently reduces the risk of infections.</font>
+
+								</div>
+
+							</div>
+
+							<div style="display: inline-block;width: auto;">
+								
+								<img src="images/health.png">
+
+							</div>
+
+						</div>
+
+					</div>
+
+					<!-- End -->
+
+					<div class="col-lg-4 col-sm-4 col-md-4 tips">
+						
+						<div class="row" style="justify-content: space-between;">
+							
+							<div style="background: transparent;display: flex;flex-direction: column;justify-content: center;" class="col-lg-6 col-sm-6 col-md-6">
+								
+								<p class="health">Health Tips</p>
+
+								<font class="did">Did you know?</font>
+
+								<div style="margin-top: 5px;">
+									
+									<font class="tipe">Washing your hands frequently reduces the risk of infections.</font>
+
+								</div>
+
+							</div>
+
+							<div style="display: inline-block;width: auto;">
+								
+								<img src="images/health.png">
+
+							</div>
+
+						</div>
+
+					</div>
+
+					<!-- End -->
+
+				</div>
+
+			</div>
+
+		</div>
+
 	</div>
 
-<!-- </div>	 -->
+</div>	
 
 </body>
 
 <script>
 	
-// 	window.addEventListener('load', function() {
-//     // After 3 seconds, fade out the splash screen
-//     setTimeout(function() {
-//         const splashScreen = document.getElementById('splash-screen');
-//         const mainContent = document.getElementById('main-content');
+	window.addEventListener('load', function() {
+    // After 3 seconds, fade out the splash screen
+    setTimeout(function() {
+        const splashScreen = document.getElementById('splash-screen');
+        const mainContent = document.getElementById('main-content');
 
-//         // Fade out the splash screen
-//         splashScreen.style.opacity = 0;
+        // Fade out the splash screen
+        splashScreen.style.opacity = 0;
 
-//         // Fade in the main content
-//         setTimeout(function() {
-//             splashScreen.style.display = 'none';
-//             mainContent.style.opacity = 1;
-//         }, 100); // Wait for 2 seconds for the fade out to complete
-//     }, 1500); // 3 seconds before the fade-out starts
-// });
+        // Fade in the main content
+        setTimeout(function() {
+            splashScreen.style.display = 'none';
+            mainContent.style.opacity = 1;
+        }, 100); // Wait for 2 seconds for the fade out to complete
+    }, 1500); // 3 seconds before the fade-out starts
+});
 
 
 </script>
